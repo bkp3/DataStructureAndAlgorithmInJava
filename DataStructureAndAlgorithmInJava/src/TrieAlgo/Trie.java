27b1ce81045884540560b8c5fd631ff637eee554
Bhola Kumar <38558383+bkp3@users.noreply.github.com>
2021-12-22 19:23:33 +0530

    Trie

--- DataStructureAndAlgorithmInJava/src/TrieAlgo/Trie.java
+++ DataStructureAndAlgorithmInJava/src/TrieAlgo/Trie.java
@@ -1,53 +1,58 @@
 package TrieAlgo;
+
 import java.util.*;
+
 public class Trie {
 
 	private TrieNode root;
-	
+
 	public Trie() {
-		root = new TrieNode(); // root is empty 
+		root = new TrieNode(); // root is empty
 	}
-	
+
 	private class TrieNode {
 		private TrieNode[] children;
 		private boolean isWord;
-		
+
 		public TrieNode() {
 			this.children = new TrieNode[26]; // storing english words - a -> z
 			this.isWord = false;
 		}
 	}
-	
+
 	public void insert(String word) {
-		
-		if(word==null || word.isEmpty()) {
-			System.out.println("Enter appropriate word....");
-		}
-		
-		word = word.toLowerCase();
-		
-		TrieNode current = root;
-		for(int i=0;i<word.length();i++) {
-			char c = word.charAt(i);
-			int index = c-'a';
-			if(current.children[index]==null) {
-				TrieNode node = new TrieNode();
-				current.children[index]=node;
-				current=node;
-			}else {
-				current=current.children[index];
+
+		TrieNode curr = root;
+
+		for (int i = 0; i < word.length(); i++) {
+			int index = word.charAt(i) - 'a';
+			if (curr.children[index] == null) {
+				curr.children[index] = new TrieNode();
 			}
+			curr = curr.children[index];
 		}
-		current.isWord=true;
+		curr.isWord = true;
+
 	}
-	
+
 	public boolean search(String word) {
-		return false;
+		TrieNode curr = root;
+		for (int i = 0; i < word.length(); i++) {
+			int index = word.charAt(i) - 'a';
+			if (curr.children[index] == null) {
+				return false;
+			}
+			curr = curr.children[index];
+		}
+		if (curr.isWord) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Trie obj = new Trie();
 		obj.insert("cat");
 		obj.insert("cab");
